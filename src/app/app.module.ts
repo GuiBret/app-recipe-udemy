e0ea519f49139002cf01408aca39d283943ab049
GuiBret <guillaume.bretzner@gmail.com>
2020-03-29 22:04:05 +0200

    Commit initial recipe book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,27 @@
+import { RecipeBookComponent } from './RecipeBook/recipe-book.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './ShoppingList/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './ShoppingList/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './RecipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './RecipeBook/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './RecipeBook/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent
   ],
   imports: [
     BrowserModule
