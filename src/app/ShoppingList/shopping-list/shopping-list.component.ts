91786a1365af4402899e68d463ec7a62014f62bd
GuiBret <guillaume.bretzner@gmail.com>
2021-01-09 16:24:08 +0100

    Handle ingredients with reducer

--- src/app/ShoppingList/shopping-list/shopping-list.component.ts
+++ src/app/ShoppingList/shopping-list/shopping-list.component.ts
@@ -1,31 +1,35 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingService } from '../shopping.service';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
-  
+
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Array<Ingredient>;
+  ingredients: Observable<{ingredients: Array<Ingredient>}>;
   private igChangedSub: Subscription;
-  constructor(private shoppingService: ShoppingService) { }
+  constructor(
+    private shoppingService: ShoppingService,
+    private store: Store<{shoppingList: {ingredients: Array<Ingredient>}}>) { }
 
   ngOnInit(): void {
 
-    this.ingredients = this.shoppingService.getIngredients();
+    this.ingredients = this.store.select('shoppingList');
+    // this.ingredients = this.shoppingService.getIngredients();
 
     this.igChangedSub = this.shoppingService.newIngredientList.subscribe((ingredients_list: Array<Ingredient>) => {
       console.log('Coucou');
-      this.ingredients = ingredients_list;
+      // this.ingredients = ingredients_list;
     })
   }
 
   addIngredientToList(new_ingredient: Ingredient) {
-    this.ingredients.push(new_ingredient);
+    // this.ingredients.push(new_ingredient);
   }
 
   ngOnDestroy() {
